c86c8ac3ffc143278a55485446b933f88fadf4fe
arunthampi <arun.thampi@gmail.com>
2016-05-11 13:03:59 -0700

    Add Botmetrics.message API

--- src/botmetrics.js
+++ src/botmetrics.js
@@ -1,4 +1,5 @@
 var HttpClient = require('scoped-http-client');
+var _ = require('lodash');
 
 var Botmetrics = {};
 
@@ -36,6 +37,74 @@ Botmetrics.registerBot = function(token, opts, callback) {
     if(err) {
       callback(err, false);
     } else if (resp.statusCode != 201) {
+      callback(new Error("Unexpected Status Code from Botmetrics API"), false);
+    } else {
+      callback(null, true);
+    }
+  });
+};
+
+Botmetrics.message = function(teamId, opts, callback) {
+  var messageOpts = {};
+
+  if(typeof opts == 'function') {
+    callback = opts;
+  } else {
+    messageOpts = opts;
+  }
+
+  var botId = (messageOpts && messageOpts['botId']) ? messageOpts['botId'] : process.env.BOTMETRICS_BOT_ID;
+  var apiKey = (messageOpts && messageOpts['apiKey']) ? messageOpts['apiKey'] : process.env.BOTMETRICS_API_KEY;
+
+  if(!botId || botId == "") {
+    callback(new Error("You have to either set the env variable BOTMETRICS_BOT_ID or pass in an as argument botId"));
+  }
+  if(!apiKey || apiKey == "") {
+    callback(new Error("You have to either set the env variable BOTMETRICS_API_KEY or pass in an as argument apiKey"));
+  }
+
+  var host = process.env.BOTMETRICS_API_HOST || 'https://www.getbotmetrics.com';
+  var url = host + "/bots/" + botId + "/messages";
+
+  var params =  {
+    message: {
+      team_id: teamId,
+    }
+  };
+
+  if (messageOpts['user'] && messageOpts['user'].length > 0) {
+    params['message']['user'] = messageOpts['user'].trim();
+  }
+
+  if (messageOpts['channel'] && messageOpts['channel'].length > 0) {
+    params['message']['channel'] = messageOpts['channel'].trim();
+  }
+
+  if (messageOpts['text'] && messageOpts['text'].length > 0) {
+    params['message']['text'] = messageOpts['text'].trim();
+  }
+
+  if (attachments = messageOpts['attachments']) {
+    if (!_.isString(attachments)) {
+      attachments = JSON.stringify(attachments);
+    }
+
+    params['message']['attachments'] = attachments;
+  }
+
+  if (!((params['message']['user'] && params['message']['user'].length > 0) ||
+       (params['message']['channel'] && params['message']['channel'].length > 0))) {
+    callback(new Error("You must specify either a 'user' or 'channel' to send the message to"), false);
+    return;
+  }
+
+  var http = HttpClient.create(url);
+  http.header('Authorization', apiKey).
+       header('Content-Type', 'application/json').
+       post(JSON.stringify(params))(function(err, resp, body) {
+    if(err) {
+      callback(err, false);
+    } else if (resp.statusCode != 202) {
       callback(new Error("Unexpected Status Code from Botmetrics API"), false);
     } else {
       callback(null, true);
--- test/botmetrics_test.js
+++ test/botmetrics_test.js
@@ -145,32 +145,282 @@ describe('Botmetrics', function() {
         });
       });
     });
-  });
 
-  context('with botId not set', function() {
-    it('should return an error', function() {
-      Botmetrics.registerBot('bot-token', function(err, status) {
-        expect(err).to.not.be.null;
-        expect(err.message).to.eql('You have to either set the env variable BOTMETRICS_BOT_ID or pass in an as argument botId');
-        expect(status).to.be.false;
+    context('with botId not set', function() {
+      it('should return an error', function() {
+        Botmetrics.registerBot('bot-token', function(err, status) {
+          expect(err).to.not.be.null;
+          expect(err.message).to.eql('You have to either set the env variable BOTMETRICS_BOT_ID or pass in an as argument botId');
+          expect(status).to.be.false;
+        });
+      });
+    });
+
+    context('with apiKey not set', function() {
+      beforeEach(function() {
+        process.env.BOTMETRICS_BOT_ID = 'botmetrics-bot-id';
+      });
+
+      afterEach(function() {
+        process.env.BOTMETRICS_BOT_ID = null;
+      });
+
+      it('should return an error', function() {
+        Botmetrics.registerBot('bot-token', function(err, status) {
+          expect(err).to.not.be.null;
+          expect(err.message).to.eql('You have to either set the env variable BOTMETRICS_API_KEY or pass in an as argument apiKey');
+          expect(status).to.be.false;
+        });
       });
     });
   });
 
-  context('with apiKey not set', function() {
-    beforeEach(function() {
-      process.env.BOTMETRICS_BOT_ID = 'botmetrics-bot-id';
+  describe('.message', function() {
+    context('with invalid params', function() {
+      beforeEach(function() {
+        process.env.BOTMETRICS_BOT_ID = 'botmetrics-bot-id';
+        process.env.BOTMETRICS_API_KEY = 'botmetrics-api-key';
+      });
+
+      afterEach(function() {
+        process.env.BOTMETRICS_BOT_ID = null;
+        process.env.BOTMETRICS_API_KEY = null;
+      });
+
+      context('without either a user or channel attribute', function() {
+        it('should make a call to the Botmetrics API sending a message', function(done) {
+          Botmetrics.message('TDEADBEEF1', {text: 'hello'}, function(err, status) {
+            expect(err).to.not.be.null;
+            expect(status).to.be.false;
+            expect(err.message).to.eql("You must specify either a 'user' or 'channel' to send the message to");
+            done();
+          });
+        });
+      });
     });
 
-    afterEach(function() {
-      process.env.BOTMETRICS_BOT_ID = null;
+    context('with environment variables set', function() {
+      var params;
+      var scope;
+      var statusCode;
+
+      beforeEach(function() {
+        process.env.BOTMETRICS_BOT_ID = 'botmetrics-bot-id';
+        process.env.BOTMETRICS_API_KEY = 'botmetrics-api-key';
+
+        scope = nock('https://www.getbotmetrics.com', {
+            reqheaders: {
+                'Authorization': 'botmetrics-api-key',
+                'Content-Type': 'application/json'
+              }
+            })
+            .post('/bots/botmetrics-bot-id/messages', params)
+            .reply(statusCode);
+      });
+
+      afterEach(function() {
+        process.env.BOTMETRICS_BOT_ID = null;
+        process.env.BOTMETRICS_API_KEY = null;
+      });
+
+      context('API returns the correct status code', function() {
+        before(function() {
+          statusCode = 202;
+        });
+
+        context('with the user param', function() {
+          before(function() {
+            params = {
+              message: {
+                team_id: 'TDEADBEEF1',
+                user: 'UDEADBEEF1',
+                text: 'hello'
+              }
+            };
+          });
+
+          it('should make a call to the Botmetrics API sending a message', function(done) {
+            Botmetrics.message('TDEADBEEF1', {user: 'UDEADBEEF1', text: 'hello' }, function(err, status) {
+              expect(status).to.be.true;
+              expect(scope.isDone()).to.be.true;
+              done();
+            });
+          });
+        });
+
+        context('with the channel param', function() {
+          before(function() {
+            params = {
+              message: {
+                team_id: 'TDEADBEEF1',
+                channel: 'CDEADBEEF1',
+                text: 'hello'
+              }
+            };
+          });
+
+          it('should make a call to the Botmetrics API sending a message', function(done) {
+            Botmetrics.message('TDEADBEEF1', {channel: 'CDEADBEEF1', text: 'hello' }, function(err, status) {
+              expect(status).to.be.true;
+              expect(scope.isDone()).to.be.true;
+              done();
+            });
+          });
+        });
+
+        context('with the attachments param (as an object)', function() {
+          before(function() {
+            params = {
+              message: {
+                team_id: 'TDEADBEEF1',
+                channel: 'CDEADBEEF1',
+                attachments: JSON.stringify([{"fallback": "hello", "text": "hello"}])
+              }
+            };
+          });
+
+          it('should make a call to the Botmetrics API sending a message', function(done) {
+            Botmetrics.message('TDEADBEEF1', {channel: 'CDEADBEEF1', attachments: [{"fallback": "hello", "text": "hello"}] }, function(err, status) {
+              expect(status).to.be.true;
+              expect(scope.isDone()).to.be.true;
+              done();
+            });
+          });
+        });
+
+        context('with the attachments param (as a String)', function() {
+          before(function() {
+            params = {
+              message: {
+                team_id: 'TDEADBEEF1',
+                channel: 'CDEADBEEF1',
+                attachments: JSON.stringify([{"fallback": "hello", "text": "hello"}])
+              }
+            };
+          });
+
+          it('should make a call to the Botmetrics API sending a message', function(done) {
+            Botmetrics.message('TDEADBEEF1', {channel: 'CDEADBEEF1', attachments: JSON.stringify([{"fallback": "hello", "text": "hello"}]) }, function(err, status) {
+              expect(status).to.be.true;
+              expect(scope.isDone()).to.be.true;
+              done();
+            });
+          });
+        });
+      });
+
+      context('API returns an unexpected status code', function() {
+        before(function() {
+          statusCode = 400;
+          params = {
+            message: {
+              team_id: 'TDEADBEEF1',
+              channel: 'CDEADBEEF1',
+              attachments: JSON.stringify([{"fallback": "hello", "text": "hello"}])
+            }
+          };
+        });
+
+        it('should make a call to the Botmetrics API registering the bot', function(done) {
+          Botmetrics.message('TDEADBEEF1', {channel: 'CDEADBEEF1', attachments: JSON.stringify([{"fallback": "hello", "text": "hello"}]) }, function(err, status) {
+            expect(err).to.not.be.null;
+            expect(err.message).to.eql('Unexpected Status Code from Botmetrics API');
+            expect(status).to.be.false;
+            expect(scope.isDone()).to.be.true;
+            done();
+          });
+        });
+      });
     });
 
-    it('should return an error', function() {
-      Botmetrics.registerBot('bot-token', function(err, status) {
-        expect(err).to.not.be.null;
-        expect(err.message).to.eql('You have to either set the env variable BOTMETRICS_API_KEY or pass in an as argument apiKey');
-        expect(status).to.be.false;
+    context('without environment variables set, but passed in correctly as params', function() {
+      var params;
+      var scope;
+      var statusCode;
+
+      beforeEach(function() {
+        params = {
+          message: {
+            team_id: 'TDEADBEEF1',
+            channel: 'CDEADBEEF1',
+            attachments: JSON.stringify([{"fallback": "hello", "text": "hello"}])
+          }
+        };
+
+        scope = nock('https://www.getbotmetrics.com', {
+            reqheaders: {
+                'Authorization': 'botmetrics-api-key',
+                'Content-Type': 'application/json'
+              }
+            })
+            .post('/bots/botmetrics-bot-id/messages', params)
+            .reply(statusCode);
+      });
+
+      context('API returns the correct status code', function() {
+        before(function() {
+          statusCode = 202;
+        });
+
+        it('should make a call to the Botmetrics API to send the message', function(done) {
+          Botmetrics.message('TDEADBEEF1', {apiKey: 'botmetrics-api-key', botId: 'botmetrics-bot-id', channel: 'CDEADBEEF1', attachments: JSON.stringify([{"fallback": "hello", "text": "hello"}]) }, function(err, status) {
+            expect(status).to.be.true;
+            expect(scope.isDone()).to.be.true;
+            done();
+          });
+        });
+      });
+
+      context('API returns an unexpected status code', function() {
+        before(function() {
+          statusCode = 400;
+
+          params = {
+            message: {
+              team_id: 'TDEADBEEF1',
+              channel: 'CDEADBEEF1',
+              attachments: JSON.stringify([{"fallback": "hello", "text": "hello"}])
+            }
+          };
+        });
+
+        it('should make a call to the Botmetrics API registering the bot', function(done) {
+          Botmetrics.message('TDEADBEEF1', {apiKey: 'botmetrics-api-key', botId: 'botmetrics-bot-id', channel: 'CDEADBEEF1', attachments: JSON.stringify([{"fallback": "hello", "text": "hello"}]) }, function(err, status) {
+            expect(err).to.not.be.null;
+            expect(err.message).to.eql('Unexpected Status Code from Botmetrics API');
+            expect(status).to.be.false;
+            expect(scope.isDone()).to.be.true;
+            done();
+          });
+        });
+      });
+    });
+
+    context('with botId not set', function() {
+      it('should return an error', function() {
+        Botmetrics.message('TDEADBEEF1', {channel: 'CDEADBEEF1', attachments: JSON.stringify([{"fallback": "hello", "text": "hello"}]) }, function(err, status) {
+          expect(err).to.not.be.null;
+          expect(err.message).to.eql('You have to either set the env variable BOTMETRICS_BOT_ID or pass in an as argument botId');
+          expect(status).to.be.false;
+        });
+      });
+    });
+
+    context('with apiKey not set', function() {
+      beforeEach(function() {
+        process.env.BOTMETRICS_BOT_ID = 'botmetrics-bot-id';
+      });
+
+      afterEach(function() {
+        process.env.BOTMETRICS_BOT_ID = null;
+      });
+
+      it('should return an error', function() {
+        Botmetrics.message('TDEADBEEF1', {channel: 'CDEADBEEF1', attachments: JSON.stringify([{"fallback": "hello", "text": "hello"}]) }, function(err, status) {
+          expect(err).to.not.be.null;
+          expect(err.message).to.eql('You have to either set the env variable BOTMETRICS_API_KEY or pass in an as argument apiKey');
+          expect(status).to.be.false;
+        });
       });
     });
   });
